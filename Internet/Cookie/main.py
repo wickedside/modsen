a571fbb8446793261714d98422d88c287f946eae
wickedside <w1ck3ds1d3@gmail.com>
2024-07-09 10:14:20 +0300

    made Internet module with Selenium using ChromeDriver

---
+++ Internet/Cookie/main.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+chrome_driver_path = '../chromedriver.exe'
+
+# Chrome options
+options = Options()
+options.add_argument("--headless")  # launching without interface (no need)
+
+# web-driver init
+service = Service(chrome_driver_path)
+driver = webdriver.Chrome(service=service, options=options)
+
+try:
+    # open web page
+    driver.get('https://example.com')
+
+    # set value in Cookie
+    driver.add_cookie({'name': 'Cookieeee:)', 'value': 'someCookieVal'})
+
+    # get value from Cookie
+    cookie = driver.get_cookie('Cookieeee:)')
+    print("Cookie value:", cookie)
+
+    # remove value from Cookie
+    driver.delete_cookie('Cookieeee:)')
+
+    # removal check
+    cookie_after_remove = driver.get_cookie('Cookieeee:)')
+    print("Cookie value after remove:", cookie_after_remove)
+except Exception as err:
+    print(f"Error: ", err)
+
+finally:
+    driver.quit()
---
+++ Internet/LocalStorage/main.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+chrome_driver_path = '../chromedriver.exe'
+
+# Chrome options
+options = Options()
+options.add_argument("--headless")  # launching without interface (no need)
+
+# web-driver init
+service = Service(chrome_driver_path)
+driver = webdriver.Chrome(service=service, options=options)
+
+try:
+    # open web page
+    driver.get('https://example.com')
+
+    # set value in LocalStorage
+    script_set = "window.localStorage.setItem('Key', 'Value');"
+    driver.execute_script(script_set)
+
+    # get value from LocalStorage
+    script_get = "return window.localStorage.getItem('Key');"
+    value = driver.execute_script(script_get)
+    print("LocalStorage value:", value)
+
+    # remove value from LocalStorage
+    script_remove = "window.localStorage.removeItem('Key');"
+    driver.execute_script(script_remove)
+
+    # removal check
+    value_after_remove = driver.execute_script(script_get)
+    print("Value after remove:", value_after_remove)
+except Exception as err:
+    print(f"Error: ", err)
+
+finally:
+    driver.quit()
